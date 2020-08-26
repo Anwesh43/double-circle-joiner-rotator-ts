2bd2137a5cfc8e1b14a8cbe3fffcd747a0399733
Anwesh43 <anweshthecool0@gmail.com>
2020-08-26 18:19:33 +0530

    adding CJRNode class

--- index.ts
+++ index.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class CJRNode {
+
+    next : CJRNode 
+    prev : CJRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CJRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCJRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CJRNode {
+        var curr : CJRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
